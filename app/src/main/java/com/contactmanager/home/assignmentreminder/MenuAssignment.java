315787140fb1e29e1e7ff2ea2fffdae00e8f23f5
shweta <shweta.agrawal15@gmail.com>
2015-08-06 22:28:16 -0700

    added code to view/add and clock reminder for an assignment.

---
+++ app/src/main/java/com/contactmanager/home/assignmentreminder/MenuAssignment.java
@@ -0,0 +1,94 @@
+package com.contactmanager.home.assignmentreminder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by home on 7/27/2015.
+ */
+
+//public class MenuAssignment extends Activity {
+
+
+
+
+
+public class MenuAssignment extends ActionBarActivity {
+    Database db = new Database(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_assignment);
+
+    }
+    public void addAssignment(View v)
+    {
+        Log.d("test", "adding");
+        //get data from form
+        EditText nameTxt = (EditText)findViewById(R.id.assignText);
+        EditText dateTxt = (EditText)findViewById(R.id.dateText);
+        EditText courseTxt = (EditText)findViewById(R.id.courseText);
+        EditText notesTxt = (EditText)findViewById(R.id.notesText);
+
+        db.open();
+        long id = db.insertRecord(nameTxt.getText().toString(), dateTxt.getText().toString(), courseTxt.getText().toString(), notesTxt.getText().toString());
+        db.close();
+
+        dateTxt.setText("");
+        courseTxt.setText("");
+        nameTxt.setText("");
+        notesTxt.setText("");
+        Toast.makeText(MenuAssignment.this,"Assignment Added", Toast.LENGTH_LONG).show();
+
+
+    }
+    public void viewAssignments(View v)
+    {
+        Intent i = new Intent(this, ViewAssignments.class);
+        startActivity(i);
+
+
+    }
+
+    public void addReminder(View v)
+    {
+        Intent i = new Intent(this, TimeReminder.class);
+        startActivity(i);
+    }
+
+};
+   /* @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_assignment);
+    }
+*/
+/* @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_menu_assignment, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+*/
---
+++ app/src/main/java/com/contactmanager/home/assignmentreminder/ViewAssignments.java
@@ -0,0 +1,92 @@
+package com.contactmanager.home.assignmentreminder;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class ViewAssignments extends ActionBarActivity {
+
+    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+    ArrayList<String> listItems=new ArrayList<String>();
+
+    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    ArrayAdapter<String> adapter;
+
+    Database db = new Database(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_assignments);
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+//        setListAdapter(adapter);
+
+
+        db.open();
+        Cursor c = db.getAllRecords();
+
+//        for (int index=0; index < cursor.getCount(); index++)
+//        {
+//
+//        }
+
+        ListView listView =(ListView)findViewById(R.id.listView);
+        c.moveToFirst();
+
+
+        int counter = 1;
+        while(! c.isAfterLast()) {
+            String temp="";
+            String s1=c.getString(0);
+            String s2=c.getString(1);
+            String s3=c.getString(2);
+            String s4=c.getString(3);
+            String s5=c.getString(4);
+            temp=temp+"\n"+s2 +"\n"+s3 +"\n"+s4 +"\n"+s5;
+            System.out.println(counter + ":::" + temp);
+
+            listItems.add(temp);
+            c.moveToNext();
+        }
+
+        db.close();
+
+        //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+        ArrayAdapter<String> adapter =new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_view_assignments, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
